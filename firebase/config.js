5a0ff9db947dceed3de14b43d6bffc852f69b93d
davegreensmith <dave_greensmith@msn.com>
2022-08-09 12:23:10 +0100

    add delete user function

--- firebase/config.js
+++ firebase/config.js
@@ -1,26 +1,14 @@
-import { initializeApp } from "firebase/app";
-import {
-  getFirestore,
-  collection,
-  getDocs,
-  addDoc,
-  setDoc,
-  doc,
-} from "firebase/firestore";
-import {
-  getAuth,
-  createUserWithEmailAndPassword,
-  signOut,
-  signInWithEmailAndPassword,
-} from "firebase/auth";
+import { initializeApp } from 'firebase/app';
+import { getFirestore, collection, getDocs, addDoc, setDoc, doc, deleteDoc } from 'firebase/firestore';
+import { getAuth, createUserWithEmailAndPassword, signOut, signInWithEmailAndPassword, UserInfo } from 'firebase/auth';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCl97mht3QLs6YOeh9ugsWba0cFE5PhFhs",
-  authDomain: "chip-in-db.firebaseapp.com",
-  projectId: "chip-in-db",
-  storageBucket: "chip-in-db.appspot.com",
-  messagingSenderId: "871906630927",
-  appId: "1:871906630927:web:7f4fb5830005d79abaf27a",
+  apiKey: 'AIzaSyCl97mht3QLs6YOeh9ugsWba0cFE5PhFhs',
+  authDomain: 'chip-in-db.firebaseapp.com',
+  projectId: 'chip-in-db',
+  storageBucket: 'chip-in-db.appspot.com',
+  messagingSenderId: '871906630927',
+  appId: '1:871906630927:web:7f4fb5830005d79abaf27a',
 };
 
 export const app = initializeApp(firebaseConfig);
@@ -29,8 +17,8 @@ export const db = getFirestore();
 export const auth = getAuth();
 
 //collection refs
-export const usersRef = collection(db, "users");
-export const errandsRef = collection(db, "errands");
+export const usersRef = collection(db, 'users');
+export const errandsRef = collection(db, 'errands');
 
 //get collection ref data
 export default function fetchUsers() {
@@ -41,20 +29,20 @@ export default function fetchUsers() {
       snapshot.docs.forEach((doc) => {
         users.push({ ...doc.data(), id: doc.id });
       });
-      console.log(users, "<<< users");
+      console.log(users, '<<< users');
     })
     .catch((err) => {
-      console.log(err.message, "<<< users errors");
+      console.log(err.message, '<<< users errors');
     });
 }
 
 // write new user to the databaseauth
 export function addUser(email, id) {
-  const userRef = doc(db, "users", id);
+  const userRef = doc(db, 'users', id);
 
   setDoc(userRef, { email })
     .then(() => {
-      console.log("users table updated");
+      console.log('users table updated');
     })
     .catch((err) => {
       console.log(err.message);
@@ -65,18 +53,12 @@ export function addUser(email, id) {
 export function signUpNewUser(email, password) {
   createUserWithEmailAndPassword(auth, email, password)
     .then((cred) => {
-      console.log(
-        cred.user.reloadUserInfo.email,
-        "<<< createUserwithPass email"
-      );
-      console.log(cred.user.reloadUserInfo.localId, "<<< auth ID");
-      return Promise.all([
-        cred.user.reloadUserInfo.email,
-        cred.user.reloadUserInfo.localId,
-      ]);
+      console.log(cred.user.reloadUserInfo.email, '<<< createUserwithPass email');
+      console.log(cred.user.reloadUserInfo.localId, '<<< auth ID');
+      return Promise.all([cred.user.reloadUserInfo.email, cred.user.reloadUserInfo.localId]);
     })
     .then(([email, id]) => {
-      console.log(email, "<<< email");
+      console.log(email, '<<< email');
       addUser(email, id);
     })
     .catch((err) => {
@@ -84,13 +66,11 @@ export function signUpNewUser(email, password) {
     });
 }
 
-signUpNewUser("test123@gmail.com", "pa55w0rd");
-
 //logging in and out
 export function userLogout() {
   signOut(auth)
     .then(() => {
-      console.log("the user logged out");
+      console.log('the user logged out');
     })
     .catch((err) => {
       console.log(err.message);
@@ -100,9 +80,22 @@ export function userLogout() {
 export function userLogin(email, password) {
   signInWithEmailAndPassword(auth, email, password)
     .then((cred) => {
-      console.log("user logged in", cred.user);
+      console.log(UserInfo(), '<<< user info');
+      console.log('user logged in', cred.user);
     })
     .catch((err) => {
       console.log(err.message);
     });
 }
+
+//delete users
+
+export function deleteUser(id) {
+  const userRef = doc(db, 'users', id);
+
+  deleteDoc(userRef).then(() => {
+    console.log('user deleted');
+  });
+}
+
+userLogin('test123@gmail.com', 'pa55w0rd');
