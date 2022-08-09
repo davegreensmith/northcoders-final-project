4954024dd0f3e501a417358955c13cd385ae0c43
davegreensmith <dave_greensmith@msn.com>
2022-08-09 12:57:58 +0100

    add errands function complete

--- firebase/config.js
+++ firebase/config.js
@@ -1,26 +1,14 @@
-import { initializeApp } from "firebase/app";
-import {
-  getFirestore,
-  collection,
-  getDocs,
-  addDoc,
-  setDoc,
-  doc,
-} from "firebase/firestore";
-import {
-  getAuth,
-  createUserWithEmailAndPassword,
-  signOut,
-  signInWithEmailAndPassword,
-} from "firebase/auth";
+import { initializeApp } from 'firebase/app';
+import { getFirestore, collection, getDocs, addDoc, setDoc, doc } from 'firebase/firestore';
+import { getAuth, createUserWithEmailAndPassword, signOut, signInWithEmailAndPassword } from 'firebase/auth';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCl97mht3QLs6YOeh9ugsWba0cFE5PhFhs",
-  authDomain: "chip-in-db.firebaseapp.com",
-  projectId: "chip-in-db",
-  storageBucket: "chip-in-db.appspot.com",
-  messagingSenderId: "871906630927",
-  appId: "1:871906630927:web:7f4fb5830005d79abaf27a",
+  apiKey: 'AIzaSyCl97mht3QLs6YOeh9ugsWba0cFE5PhFhs',
+  authDomain: 'chip-in-db.firebaseapp.com',
+  projectId: 'chip-in-db',
+  storageBucket: 'chip-in-db.appspot.com',
+  messagingSenderId: '871906630927',
+  appId: '1:871906630927:web:7f4fb5830005d79abaf27a',
 };
 
 export const app = initializeApp(firebaseConfig);
@@ -29,8 +17,8 @@ export const db = getFirestore();
 export const auth = getAuth();
 
 //collection refs
-export const usersRef = collection(db, "users");
-export const errandsRef = collection(db, "errands");
+export const usersRef = collection(db, 'users');
+export const errandsRef = collection(db, 'errands');
 
 //get collection ref data
 export default function fetchUsers() {
@@ -41,20 +29,20 @@ export default function fetchUsers() {
       snapshot.docs.forEach((doc) => {
         users.push({ ...doc.data(), id: doc.id });
       });
-      console.log(users, "<<< users");
+      console.log(users, '<<< users');
     })
     .catch((err) => {
-      console.log(err.message, "<<< users errors");
+      console.log(err.message, '<<< users errors');
     });
 }
 
 // write new user to the databaseauth
 export function addUser(email, id) {
-  const userRef = doc(db, "users", id);
+  const userRef = doc(db, 'users', id);
 
   setDoc(userRef, { email })
     .then(() => {
-      console.log("users table updated");
+      console.log('users table updated');
     })
     .catch((err) => {
       console.log(err.message);
@@ -65,18 +53,12 @@ export function addUser(email, id) {
 export function signUpNewUser(email, password) {
   createUserWithEmailAndPassword(auth, email, password)
     .then((cred) => {
-      console.log(
-        cred.user.reloadUserInfo.email,
-        "<<< createUserwithPass email"
-      );
-      console.log(cred.user.reloadUserInfo.localId, "<<< auth ID");
-      return Promise.all([
-        cred.user.reloadUserInfo.email,
-        cred.user.reloadUserInfo.localId,
-      ]);
+      console.log(cred.user.reloadUserInfo.email, '<<< createUserwithPass email');
+      console.log(cred.user.reloadUserInfo.localId, '<<< auth ID');
+      return Promise.all([cred.user.reloadUserInfo.email, cred.user.reloadUserInfo.localId]);
     })
     .then(([email, id]) => {
-      console.log(email, "<<< email");
+      console.log(email, '<<< email');
       addUser(email, id);
     })
     .catch((err) => {
@@ -84,13 +66,13 @@ export function signUpNewUser(email, password) {
     });
 }
 
-signUpNewUser("test123@gmail.com", "pa55w0rd");
+// signUpNewUser('test123@gmail.com', 'pa55w0rd');
 
 //logging in and out
 export function userLogout() {
   signOut(auth)
     .then(() => {
-      console.log("the user logged out");
+      console.log('the user logged out');
     })
     .catch((err) => {
       console.log(err.message);
@@ -100,9 +82,37 @@ export function userLogout() {
 export function userLogin(email, password) {
   signInWithEmailAndPassword(auth, email, password)
     .then((cred) => {
-      console.log("user logged in", cred.user);
+      console.log('user logged in', cred.user);
     })
     .catch((err) => {
       console.log(err.message);
     });
 }
+
+//add errands to database
+
+export function addErrand(errandDetails) {
+  const errandRef = collection(db, 'errands');
+  // const errandDetails = { id, description, dueDate, errandName, location, requirements, timeframe, type };
+
+  addDoc(errandRef, errandDetails)
+    .then((mystery) => {
+      // console.log(mystery._key.path.segments[1], '<<< errand doc number');
+      // console.log(mystery.firestore._firestoreClient.user.uid, '<<< users UID');
+      console.log('users table updated');
+    })
+    .catch((err) => {
+      console.log(err.message);
+    });
+}
+
+addErrand({
+  author: 'Jon',
+  description: 'take a load of junk to the tip',
+  dueDate: '15/8/2022',
+  errandName: 'Tip run',
+  location: 'Macc',
+  requirements: 'No sense of smell',
+  timeframe: '1hr',
+  type: 'Heavy lifting',
+});
