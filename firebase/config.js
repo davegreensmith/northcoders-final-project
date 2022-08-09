4b345d23cc0e0038ec4db4dc489bfd99e9d171ab
Janash37 <janash_37@hotmail.co.uk>
2022-08-09 15:06:28 +0100

    add update errand functionality

--- firebase/config.js
+++ firebase/config.js
@@ -1,14 +1,28 @@
-import { initializeApp } from 'firebase/app';
-import { getFirestore, collection, getDocs, addDoc, setDoc, doc, updateDoc } from 'firebase/firestore';
-import { getAuth, createUserWithEmailAndPassword, signOut, signInWithEmailAndPassword, onAuthStateChanged } from 'firebase/auth';
+import { initializeApp } from "firebase/app";
+import {
+  getFirestore,
+  collection,
+  getDocs,
+  addDoc,
+  setDoc,
+  doc,
+  updateDoc,
+} from "firebase/firestore";
+import {
+  getAuth,
+  createUserWithEmailAndPassword,
+  signOut,
+  signInWithEmailAndPassword,
+  onAuthStateChanged,
+} from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyCl97mht3QLs6YOeh9ugsWba0cFE5PhFhs',
-  authDomain: 'chip-in-db.firebaseapp.com',
-  projectId: 'chip-in-db',
-  storageBucket: 'chip-in-db.appspot.com',
-  messagingSenderId: '871906630927',
-  appId: '1:871906630927:web:7f4fb5830005d79abaf27a',
+  apiKey: "AIzaSyCl97mht3QLs6YOeh9ugsWba0cFE5PhFhs",
+  authDomain: "chip-in-db.firebaseapp.com",
+  projectId: "chip-in-db",
+  storageBucket: "chip-in-db.appspot.com",
+  messagingSenderId: "871906630927",
+  appId: "1:871906630927:web:7f4fb5830005d79abaf27a",
 };
 
 export const app = initializeApp(firebaseConfig);
@@ -17,15 +31,9 @@ export const db = getFirestore();
 export const auth = getAuth();
 // const [user, setUser] = useState(null);
 
-useEffect(() => {
-  authContextProvider().then(({ user }) => {
-    console.log(user);
-  });
-}, []);
-
 //collection refs
-export const usersRef = collection(db, 'users');
-export const errandsRef = collection(db, 'errands');
+export const usersRef = collection(db, "users");
+export const errandsRef = collection(db, "errands");
 
 //get collection ref data
 export default function fetchUsers() {
@@ -36,20 +44,20 @@ export default function fetchUsers() {
       snapshot.docs.forEach((doc) => {
         users.push({ ...doc.data(), id: doc.id });
       });
-      console.log(users, '<<< users');
+      console.log(users, "<<< users");
     })
     .catch((err) => {
-      console.log(err.message, '<<< users errors');
+      console.log(err.message, "<<< users errors");
     });
 }
 
 // write new user to the databaseauth
 export function addUser(email, id) {
-  const userRef = doc(db, 'users', id);
+  const userRef = doc(db, "users", id);
 
   setDoc(userRef, { email })
     .then(() => {
-      console.log('users table updated');
+      console.log("users table updated");
     })
     .catch((err) => {
       console.log(err.message);
@@ -60,12 +68,18 @@ export function addUser(email, id) {
 export function signUpNewUser(email, password) {
   createUserWithEmailAndPassword(auth, email, password)
     .then((cred) => {
-      console.log(cred.user.reloadUserInfo.email, '<<< createUserwithPass email');
-      console.log(cred.user.reloadUserInfo.localId, '<<< auth ID');
-      return Promise.all([cred.user.reloadUserInfo.email, cred.user.reloadUserInfo.localId]);
+      console.log(
+        cred.user.reloadUserInfo.email,
+        "<<< createUserwithPass email"
+      );
+      console.log(cred.user.reloadUserInfo.localId, "<<< auth ID");
+      return Promise.all([
+        cred.user.reloadUserInfo.email,
+        cred.user.reloadUserInfo.localId,
+      ]);
     })
     .then(([email, id]) => {
-      console.log(email, '<<< email');
+      console.log(email, "<<< email");
       addUser(email, id);
     })
     .catch((err) => {
@@ -73,22 +87,23 @@ export function signUpNewUser(email, password) {
     });
 }
 
-//logging in and out
+//logging out
 export function userLogout() {
   signOut(auth)
     .then(() => {
-      console.log('the user logged out');
+      console.log("the user logged out");
     })
     .catch((err) => {
       console.log(err.message);
     });
 }
 
+//logging in
 export function userLogin(email, password) {
   signInWithEmailAndPassword(auth, email, password)
     .then((cred) => {
-      console.log('user logged in', cred.user);
-      return { msg: 'working' };
+      console.log("user logged in", cred.user);
+      return { msg: "working" };
     })
     .catch((err) => {
       console.log(err.message);
@@ -96,16 +111,14 @@ export function userLogin(email, password) {
 }
 
 //add errands to database
-
 export function addErrand(errandDetails) {
-  const errandRef = collection(db, 'errands');
-  // const errandDetails = { id, description, dueDate, errandName, location, requirements, timeframe, type };
+  const errandRef = collection(db, "errands");
 
   addDoc(errandRef, errandDetails)
     .then((mystery) => {
       // console.log(mystery._key.path.segments[1], '<<< errand doc number');
       // console.log(mystery.firestore._firestoreClient.user.uid, '<<< users UID');
-      console.log('users table updated');
+      console.log("users table updated");
     })
     .catch((err) => {
       console.log(err.message);
@@ -114,19 +127,40 @@ export function addErrand(errandDetails) {
 
 //delete users
 export function deleteUser(id) {
-  const userRef = doc(db, 'users', id);
-  
+  const userRef = doc(db, "users", id);
+
   deleteDoc(userRef).then(() => {
-    console.log('user deleted');
+    console.log("user deleted");
+  });
+}
 
 //update user info
 export function updateUserInfo() {
-  const userRef = doc(db, 'users', auth.currentUser.reloadUserInfo.localId);
+  const userRef = doc(db, "users", auth.currentUser.reloadUserInfo.localId);
 
   updateDoc(userRef, {
-    fname: 'Jan',
+    fname: "Jan",
   }).then(() => {
-    console.log('user updated');
-
+    console.log("user updated");
   });
 }
+
+//update errands
+export function updateErrand(errandID, updateBody) {
+  const errandRef = doc(db, "errands", errandID);
+
+  updateDoc(errandRef, updateBody).then(() => {
+    console.log("errand updated");
+  });
+}
+
+updateErrand("jTUz4CRGZPT9vSS3YNiM", {
+  author: "saleh",
+  description: "new description",
+  dueDate: "yesterday",
+  errandName: "really difficult task",
+  location: "liverpool",
+  requirements: "none",
+  timeframe: "2 days",
+  type: "easy work",
+});
