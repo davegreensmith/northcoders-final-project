62bdb9691056f12b3c876af0452fd9e5e670e881
Hayley Brinicombe <103518155+HayleyBrinicombe@users.noreply.github.com>
2022-08-12 16:56:25 +0100

    completed notification-screen for MVP

--- components/NotficationsScreen.jsx
+++ components/NotficationsScreen.jsx
@@ -6,7 +6,7 @@ import {
   TextInput,
   FlatList,
   Pressable,
-  StyleSheet,
+  StyleSheet
 } from "react-native";
 import { useState } from "react";
 import Header from "./Header";
@@ -14,22 +14,46 @@ import NavBar from "./NavBar";
 
 export default function NotificationsScreen({ navigation }) {
   const [notificationsList, setNotificationsList] = useState([
-    "someone has offered to help",
-    "you have a new review",
-    "you have an errand update",
-    "you have been reported for spitting",
+    "Someone has offered to help",
+    "You have a new review",
+    "You have an errand update",
+    "You have been reported for spitting"
   ]);
 
   return (
     <View style={{ flex: 1 }}>
       <Header navigation={navigation} />
-      <View style={styles.pageContent}></View>
+      <View style={styles.pageContent}>
+        {notificationsList.map((notification) => {
+          return (
+            <View style={styles.notificationContainer}>
+              <Text style={{ fontSize: 20 }}>{notification}</Text>
+            </View>
+          );
+        })}
+        <View></View>
+      </View>
       <NavBar navigation={navigation} />
     </View>
   );
 }
+
 const styles = StyleSheet.create({
   pageContent: {
-    flex: 1,
+    flex: 1
   },
+  notificationContainer: {
+    width: "85%",
+    flex: 1,
+    borderWidth: 2,
+    borderRadius: 30,
+    backgroundColor: "#FFF",
+    paddingTop: 20,
+    paddingBottom: 20,
+    paddingLeft: 15,
+    paddingRight: 15,
+    margin: 25,
+    justifyContent: "center",
+    alignItems: "center"
+  }
 });
