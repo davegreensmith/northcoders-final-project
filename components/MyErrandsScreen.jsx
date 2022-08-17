a7d65fd322b95443593f29affce84ad1e3709488
Janash37 <janash_37@hotmail.co.uk>
2022-08-17 11:10:34 +0100

    add complete errand functionality

--- components/MyErrandsScreen.jsx
+++ components/MyErrandsScreen.jsx
@@ -28,6 +28,7 @@ export default function MyErrandsScreen({ navigation }) {
   const [myErrands, setMyErrands] = useState([]);
   const [refreshPage, setRefreshPage] = useState(true);
   const [completed, setCompleted] = useState(false);
+  const [id, setId] = useState(null);
 
   const [completeButtonPressed, setCompleteButtonPressed] = useState(false);
   const [editButtonPressed, setEditButtonPressed] = useState(false);
@@ -43,8 +44,18 @@ export default function MyErrandsScreen({ navigation }) {
     });
   }
 
-  function handleCompleteErrand(errandID) {
+  function handleCompleteErrand(id) {
     setCompleted(true);
+    setId(id);
+    // handleDeleteErrand(errandID);
+    // const newList = myErrands.filter((errand) => {
+    //   return errand.errandID !== errandID;
+    // });
+    // setMyErrands([...newList]).then(() => {
+    //   console.log(errandID);
+    //   console.log(myErrands);
+    // });
+    // delete errand via config function
   }
 
   function giveKudos(id) {
@@ -95,25 +106,42 @@ export default function MyErrandsScreen({ navigation }) {
                 <View style={styles.jobLengthField}>
                   <Text>Job length: {errand.timeFrame}</Text>
                 </View>
-                {/* <View style={styles.jobLengthField}>
-                  <Text style={{ fontWeight: "bold" }}>Volunteers:</Text>
+                <View style={styles.jobLengthField}>
+                  <Text style={{ fontWeight: "bold", marginBottom: 5 }}>
+                    Volunteers:
+                  </Text>
                   {errand.chippers.map((chipper) => {
                     return (
                       <View key={chipper.id} style={styles.chipperList}>
                         <Text>{chipper.user}</Text>
-                        <Pressable
-                          disabled={false}
-                          style={styles.kudosButton}
-                          onPress={(e) => {
-                            giveKudos(chipper.id);
-                          }}
-                        >
-                          <Text>Give kudos!</Text>
-                        </Pressable>
+                        {completed && errand.errandID === id ? (
+                          <Pressable
+                            disabled={false}
+                            style={styles.kudosButton}
+                            onPress={(e) => {
+                              giveKudos(chipper.id);
+                            }}
+                          >
+                            <Text>Give kudos!</Text>
+                          </Pressable>
+                        ) : (
+                          <></>
+                        )}
                       </View>
                     );
                   })}
-                </View> */}
+                </View>
+                {completed && errand.errandID === id ? (
+                  <View style={styles.completed}>
+                    <Text>
+                      Don't forget to give out kudos to those that helped you!
+                    </Text>
+                  </View>
+                ) : (
+                  <View>
+                    <></>
+                  </View>
+                )}
                 <View style={styles.buttonsFlexBox}>
                   <Pressable
                     style={
@@ -121,10 +149,12 @@ export default function MyErrandsScreen({ navigation }) {
                         ? styles.completeButtonPressed
                         : styles.completeButton
                     }
+                    onPress={() => {
+                      handleCompleteErrand(errand.errandID);
+                    }}
                     onPressIn={() => setCompleteButtonPressed(true)}
                     onPressOut={() => {
                       setCompleteButtonPressed(false);
-                      handleCompleteErrand(errand.errandID);
                     }}
                   >
                     <Text>Completed</Text>
@@ -197,8 +227,8 @@ const styles = StyleSheet.create({
     borderBottomWidth: 1,
   },
   titleField: {
-    justifyContent: "center",
-
+    justifyContent: "flex-start",
+    flexDirection: "row",
     padding: 15,
   },
   descriptionField: {
@@ -350,5 +380,11 @@ const styles = StyleSheet.create({
     margin: 10,
     borderWidth: 0.5,
     borderColor: "gray",
+  },
+  completed: {
+    display: "row",
+    alignItems: "center",
+    justifyContent: "center",
+    marginTop: 15,
   },
 });
